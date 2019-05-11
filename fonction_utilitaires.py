1f9152e8fd8bcf81631043d9eb4130f46f72de6c
Badbeef1 <malboeufsimon@gmail.com>
2019-05-11 16:00:16 -0400

    fonction trouver emotion

--- fonction_utilitaires.py
+++ fonction_utilitaires.py
@@ -2,10 +2,13 @@ import numpy as np
 import cv2
 import imutils
 from keras.models import load_model
+from keras.preprocessing.image import img_to_array
 #variables de classes
 face_cascade = cv2.CascadeClassifier('haarcascade_files/haarcascade_frontalface_default.xml')
 emotion_model_path = 'models/_mini_XCEPTION.56-0.64.hdf5'
 emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["fache" ,"degouter","effrayer", "heureux", "triste", "surpris",
+ "neutre"]
 def detect_face(img):
 
 
@@ -39,3 +42,44 @@ def adj_detect_face(img):
         cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 255, 255), 2)
 
     return face_img
+
+#cette fonction recoit en paramètre un image et permet d'afficer le visage
+def trouver_emotion(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+                                            flags=cv2.CASCADE_SCALE_IMAGE)
+
+    canvas = np.zeros((250, 300, 3), dtype="uint8")
+    frameClone = frame.copy()
+    if len(faces) > 0:
+        faces = sorted(faces, reverse=True,
+                       key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+        (fX, fY, fW, fH) = faces
+        # Extract the ROI of the face from the grayscale image, resize it to a fixed 28x28 pixels, and then prepare
+        # the ROI for classification via the CNN
+        roi = gray[fY:fY + fH, fX:fX + fW]
+        roi = cv2.resize(roi, (48, 48))
+        roi = roi.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+
+        preds = emotion_classifier.predict(roi)[0]
+        emotion_probability = np.max(preds)
+        label = EMOTIONS[preds.argmax()]
+
+        for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
+            # construct the label text
+            text = "{}: {:.2f}%".format(emotion, prob * 100)
+
+            w = int(prob * 300)
+            cv2.rectangle(canvas, (7, (i * 35) + 5),
+                          (w, (i * 35) + 35), (0, 0, 255), -1)
+            cv2.putText(canvas, text, (10, (i * 35) + 23),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45,
+                        (255, 255, 255), 2)
+
+        cv2.putText(frameClone, label, (fX, fY - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
+        cv2.imshow('your_face', frameClone)
+        cv2.imshow("Probabilities", canvas)
--- test_open_cv.py
+++ test_open_cv.py
@@ -5,6 +5,7 @@ from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 from fonction_utilitaires import detect_face
 from fonction_utilitaires import adj_detect_face
+from fonction_utilitaires import trouver_emotion
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,7 +20,7 @@ EMOTIONS = ["fache" ,"degouter","effrayer", "heureux", "triste", "surpris",
  "neutre"]
 
 # Tout dépendament de l'ordi la valeur peut changer entre 0 et 1 (webcam #1 ou 2/ avant ou arrière etc.)
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 # cap.isOpened nous permet de s'assurer que la caméra est ouverte, donc ça réduit les risques d'envoyer une image null à ma fonction
 while(cap.isOpened()):
@@ -27,48 +28,7 @@ while(cap.isOpened()):
 
     if ret:
         frame = imutils.resize(frame, width=300)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_detection.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
-                                                flags=cv2.CASCADE_SCALE_IMAGE)
-
-        canvas = np.zeros((250, 300, 3), dtype="uint8")
-        frameClone = frame.copy()
-        if len(faces) > 0:
-            faces = sorted(faces, reverse=True,
-                           key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
-            (fX, fY, fW, fH) = faces
-            # Extract the ROI of the face from the grayscale image, resize it to a fixed 28x28 pixels, and then prepare
-            # the ROI for classification via the CNN
-            roi = gray[fY:fY + fH, fX:fX + fW]
-            roi = cv2.resize(roi, (48, 48))
-            roi = roi.astype("float") / 255.0
-            roi = img_to_array(roi)
-            roi = np.expand_dims(roi, axis=0)
-
-            preds = emotion_classifier.predict(roi)[0]
-            emotion_probability = np.max(preds)
-            label = EMOTIONS[preds.argmax()]
-
-            for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
-                # construct the label text
-                text = "{}: {:.2f}%".format(emotion, prob * 100)
-
-                # draw the label + probability bar on the canvas
-                # emoji_face = feelings_faces[np.argmax(preds)]
-
-
-                w = int(prob * 300)
-                cv2.rectangle(canvas, (7, (i * 35) + 5),
-                              (w, (i * 35) + 35), (0, 0, 255), -1)
-                cv2.putText(canvas, text, (10, (i * 35) + 23),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.45,
-                            (255, 255, 255), 2)
-
-            cv2.putText(frameClone, label, (fX, fY - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-            cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
-            cv2.imshow('your_face', frameClone)
-            cv2.imshow("Probabilities", canvas)
+        trouver_emotion(frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
